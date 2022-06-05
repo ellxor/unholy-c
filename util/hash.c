8d4760a20bdd7b7d1e47f8926e21d8933dc3c99e
ellxor <ellxor@protonmail.ch>
2022-06-05 18:15:08 +0100

    Add hashing to util

---
+++ util/hash.c
@@ -0,0 +1,33 @@
+#include "hash.h"
+
+// murmur2 hash function
+unsigned hash(const char *in, int length) {
+	unsigned digest = 0;
+	enum { shuffle = 0x5bd1e995 };
+
+	for (int i = 0; i < length; i += 4) {
+		unsigned block = *(unsigned *)in;
+
+		block *= shuffle;
+		block ^= block >> 24;
+		block *= shuffle;
+
+		digest *= shuffle;
+		digest ^= block;
+
+		in += 4;
+	}
+
+	switch (length & 3) {
+		case 3: digest ^= in[2] << 16;
+		case 2: digest ^= in[1] << 8;
+		case 1: digest ^= in[0];
+		        digest *= shuffle;
+	}
+
+	digest ^= digest >> 13;
+	digest *= shuffle;
+	digest ^= digest >> 15;
+
+	return digest;
+}
---
+++ util/hash.h
@@ -0,0 +1,6 @@
+#ifndef HASH_H_
+#define HASH_H_
+
+unsigned hash(const char *, int);
+
+#endif //HASH_H_
