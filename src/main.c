e8800343e7c8d75ff85ebb67e3b4ab9af2bc9eab
ellxor <ellxor@protonmail.ch>
2022-06-15 23:37:36 +0100

    No longer print lexer tokens

--- src/main.c
+++ src/main.c
@@ -10,25 +10,6 @@
 #include "util/vec.h"
 
 static
-void dump_token(struct Token token) {
-	switch (token.type) {
-		case INT:		printf("INT(%d)\n", token.value); break;
-		case STRING:		printf("STRING(%s)\n", token.text); break;
-		case IDENT:	printf("IDENT(%s)\n", token.text); break;
-
-		case PUNCTUATION:
-			if (token.value > 0xff)
-				printf("PUNC(%d)\n", token.value);
-			else
-				printf("PUNC(`%c`)\n", token.value);
-			break;
-
-		default:
-			printf("unimplemented!\n");
-	}
-}
-
-static
 void print_type(struct ExprNode *node) {
 	switch (node->token->type) {
 		case KEYWORD_BOOL: printf("bool"); break;
@@ -117,16 +98,11 @@ int main() {
 	struct Token *buffer = tokens.mem;
 	int count = tokens.length;
 
-	for (int i = 0; i < count; i++) {
-		struct Token token = buffer[i];
-		dump_token(token);
-	}
-
 	struct Parser parser = { buffer, count, &allocator, false };
 	struct DeclNode *decl = parse_declaration(&parser);
 	assert(decl != NULL);
 
-	puts("\nAST DONE !!!\n");
+	puts("\n!!! AST DONE !!!\n");
 
 	if (decl->constant) {
 		assert(decl->expr->type == LITERAL);
